c868b40943675c61b6c28c337c4694a7904b7695
jprescher <jprescher@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-12 13:14:11 +0000

    = 2.3.3 = * PHP short tags converted to long open tag to prevent conflict.
    git-svn-id: https://plugins.svn.wordpress.org/facebook-events-importer/trunk@1158812 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ assets/includes/fb-sch.php
@@ -0,0 +1,23 @@
+<?php
+add_action( 'wp', 'fbe_cron_setup_schedule' );
+
+function fbe_cron_setup_schedule() {
+	if ( ! wp_next_scheduled( 'fbe_cron_hourly_event' ) ) {
+		wp_schedule_event( time(), 'hourly', 'fbe_cron_hourly_event');
+	}
+}
+
+
+add_action( 'fbe_cron_hourly_event', 'fbe_cron_do_this_hourly' );
+
+function fbe_cron_do_this_hourly() {
+	// Check for new events every hour
+	$pages = get_option("facebook_pages");
+	$location = array_filter(explode(",",$pages));
+
+	foreach ($location as $loc){
+		fbe_facebook_sdk($loc,get_option("app_id"),get_option("app_secret"));
+	}
+	update_option('fbe_cron_date', time());
+}
+?>
