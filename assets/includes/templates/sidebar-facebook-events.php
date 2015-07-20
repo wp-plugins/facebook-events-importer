79f5b448e94a5210aa428d1d45587622ff7ca1ed
jprescher <jprescher@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-20 15:31:10 +0000

    2.3.7 update
    git-svn-id: https://plugins.svn.wordpress.org/facebook-events-importer/trunk@1202376 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- assets/includes/templates/sidebar-facebook-events.php
+++
@@ -1,8 +0,0 @@
-<?php
-$fbe800 = get_option("facebook_events_pro_version");  if($fbe800){
-if ( ! is_active_sidebar( 'facebook-events' ) ) {
-	return;
-}
-dynamic_sidebar( 'facebook-events' );
-}else{	exit; }
- ?>
